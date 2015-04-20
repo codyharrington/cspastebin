00b006805ee9597c94e6ec56ea93613ae64dc61c
Cody Harrington <cody.harrington@pegasus.org.nz>
2015-04-21 11:38:55 +1200

    Jesus

--- PasteBin/Global.asax.cs
+++ PasteBin/Global.asax.cs
@@ -15,7 +15,7 @@ namespace PasteBin
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
