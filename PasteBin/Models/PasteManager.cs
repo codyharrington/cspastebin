388f1bf5bddb7dc24467378cb9b55dde702ab018
Cody Harrington <cody.harrington@pegasus.org.nz>
2015-04-23 16:48:57 +1200

    Added paste manager

---
+++ PasteBin/Models/PasteManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PasteBin.Models
+{
+    public class PasteManager
+    {
+        public static string StorePaste(Paste p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string GetPaste(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
